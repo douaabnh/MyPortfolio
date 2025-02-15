22d241b48b831b279bfab6dad59a71052b6af50f
douaabnh <douaa.benhamida8@gmail.com>
2025-02-15 16:08:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ menuIcon.addEventListener("click", () => {
     navbar.classList.toggle("active"); // Toggle menu visibility
 });
 
+function sendEmail() {
+    window.location.href = "mailto:douaa.benhamida8@gmail.com?subject=Subject%20Here&body=Hello,%20I%20would%20like%20to%20contact%20you.";
+}
 
